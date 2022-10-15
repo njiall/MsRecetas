1fb8836f2dfa2a8bbadab1368c0b667b675a6229
Ruben <ruben.astudillo@gmail.com>
2022-10-15 11:02:58 +0200

    Creamos la entidad de receta

---
+++ recetario/src/main/java/com/recetario/repository/entities/Receta.java
@@ -0,0 +1,141 @@
+package com.recetario.repository.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="recetas")
+public class Receta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto Inc en MariaDB
+	private Long id;
+	
+	private String nombre;
+	
+	private String categoria;
+	
+	//TODO Crear etiquetas
+	
+	private String ingredientes;
+	
+	private String instrucciones;
+	
+	private String notas;
+	
+		
+
+	public Receta() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Receta other = (Receta) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the categoria
+	 */
+	public String getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	/**
+	 * @return the ingredientes
+	 */
+	public String getIngredientes() {
+		return ingredientes;
+	}
+
+	/**
+	 * @param ingredientes the ingredientes to set
+	 */
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	/**
+	 * @return the instrucciones
+	 */
+	public String getInstrucciones() {
+		return instrucciones;
+	}
+
+	/**
+	 * @param instrucciones the instrucciones to set
+	 */
+	public void setInstrucciones(String instrucciones) {
+		this.instrucciones = instrucciones;
+	}
+
+	/**
+	 * @return the notas
+	 */
+	public String getNotas() {
+		return notas;
+	}
+
+	/**
+	 * @param notas the notas to set
+	 */
+	public void setNotas(String notas) {
+		this.notas = notas;
+	}
+	
+	
+
+}
